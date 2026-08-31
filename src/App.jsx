0c8d3439232f90d43dd04c6f4d4b58910ce863ec
rajkumar <rema1191@gmail.com>
2026-08-31 20:40:06 +0530

    feat: add axios for API calls and implement movie data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import "./App.css";
 import About from "./components/About";
 import AddMovie from "./components/AddMovie";
 import AddMovieByReducer from "./components/AddMovieByReducer";
+
+import axios from "axios";
 
 const App = () => {
   let movies = [
@@ -28,6 +30,54 @@ const App = () => {
 
   const title = "React Tutorial";
   const year = 2026;
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        const movies = data.map(({ id, title }) => ({
+          id: id,
+          name: title,
+          year: 2025,
+        }));
+
+        setMovieData(movies);
+      });
+  }, []);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+      );
+      const data = await response.json();
+      const movies = data.map(({ id, title }) => ({
+        id: id,
+        name: title,
+        year: 2025,
+      }));
+      console.log(movies);
+      setMovieData(movies);
+    };
+    loadMovies();
+  }, []);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users",
+      );
+
+      const movies = response.data.map(({ id, name }) => ({
+        id: id,
+        name: name,
+        year: 2025,
+      }));
+      console.log(movies);
+      setMovieData(movies);
+    };
+    loadMovies();
+  }, []);
 
   const handleClick = () => {
     setMovieTitle("");
